b20961a346d3c31f9e31bee4af71b2afb044884f
krongkan <yaakl2k@gmail.com>
2018-02-22 09:59:42 +0700

    Update Gemfile for Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: "hello, world!"
+    render html: "hello, world!1111"
   end
 end
