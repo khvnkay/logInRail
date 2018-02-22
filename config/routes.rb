9393665bf7159688e8a619ac9d11186b3ff75eb4
krongkan <yaakl2k@gmail.com>
2018-02-22 11:13:48 +0700

    static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  resources :microposts
-  resources :users
-	root 'users#index'
-	root 'application#hello'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get  'static_pages/home'
+  get  'static_pages/help'
+  get  'static_pages/about'
 end
