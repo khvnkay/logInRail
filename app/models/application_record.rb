5974957a7d7d8b44da4fa2c4b3cc78b5be8bb958
krongkan <yaakl2k@gmail.com>
2018-02-22 10:31:37 +0700

    lol

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+
+  class User < ApplicationRecord
+  validates :name,  presence: true
+  validates :email, presence: true
+end
 end
