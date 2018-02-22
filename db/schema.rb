b5c101ba9e4881acb4630c3cba61f9d9d90ebaec
krongkan <yaakl2k@gmail.com>
2018-02-22 15:29:46 +0700

    dfdf

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180222080214) do
+ActiveRecord::Schema.define(version: 20180222080310) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
